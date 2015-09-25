1a9feef10cff3062bad811d98083534a7cd23ac0
Ike <gogreen37@me.com>
2015-09-25 10:27:45 -0700

    add toggleInClass() to StudentsController.js.

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -1,3 +1,12 @@
 signIn.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsFactory) {
-    
+    $scope.students = StudentsFactory.students;
+    $scope.StudentsFactory = StudentsFactory;
+
+    $scope.toggleInClass = function(student) {
+        if (student.in_class == true) {
+            stuent.in_class = false;
+        } else if (student.in_class == false) {
+            student.in_class = true;
+        }
+    }
 })
