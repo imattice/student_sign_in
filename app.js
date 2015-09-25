b276a119e5cdbfb0dbb582145ed859124d40ee5f
Ike <gogreen37@me.com>
2015-09-25 10:10:25 -0700

    add StudentsFactory.js with sample students.

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var signIn = angular.module('signIn', ['ui.router']);
 
+//renders home page and inserts home.html partial into the index.HTML
 signIn.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider.state('home', {
         url: '',
         templateUrl:'partials/home.html'
+    });
+
+//enables students button to navigate to the students.html partial
+    $stateProvider.state('students', {
+        url:'/students',
+        templateUrl: 'partials/students.html',
+        controller: 'StudentsCtrl'
     });
 });
