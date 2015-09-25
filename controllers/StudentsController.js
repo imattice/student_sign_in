69a433508b1e0742bece476c57d67187bd80237a
Ike <gogreen37@me.com>
2015-09-25 11:47:56 -0700

    add toggleInClass() to buttons.

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -4,7 +4,7 @@ signIn.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsFactory)
 
     $scope.toggleInClass = function(student) {
         if (student.in_class == true) {
-            stuent.in_class = false;
+            student.in_class = false;
         } else if (student.in_class == false) {
             student.in_class = true;
         }
--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -2,11 +2,11 @@ signIn.factory('StudentsFactory', function StudentsFactory() {
     var factory = {};
     factory.students = [
         //sample students
-        {name: 'Ike Mattice', id: 1, in_class: true},
+        {name: 'Ike Mattice', id: 1, in_class: true },
         {name: 'Bernie Sanders', id: 2, in_class: true },
         {name: 'Hilary Clinton', id: 3, in_class: false },
         {name: 'Scott Walker', id: 4, in_class: false },
-        {name: 'Donald Trump', id: 5, in_class: true }
+        {name: 'Donald Trump', id: 5, in_class: true },
     ]
 
     return factory;
