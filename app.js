5aec457784e688bca02ec40bc33cd2c19e0332e6
Ike <gogreen37@me.com>
2015-09-25 09:36:51 -0700

    add first state in app.js.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+var signIn = angular.module('signIn', ['ui.router']);
 
+courseRoster.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('home', {
+        url: '',
+        templateUrl:'partials/home.html'
+    });
+});
