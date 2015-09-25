d513bf202557203e21bbb381f64252a332597710
Ike <gogreen37@me.com>
2015-09-25 09:45:18 -0700

    add home.html.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var signIn = angular.module('signIn', ['ui.router']);
 
-courseRoster.config(function($stateProvider, $urlRouterProvider) {
+signIn.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider.state('home', {
         url: '',
         templateUrl:'partials/home.html'
